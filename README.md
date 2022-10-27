fa9497c28a7e05488e636e80dada155067892a2e
dhananjaichand98 <dcdevdhan@gmail.com>
2022-10-26 19:54:04 -0700

    added the source code

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# HadoopInvertedIndexGenerator
-Programs that generate inverted indexes for a large corpus of documents using Map Reduce
+<a name="readme-top"></a>
+# Hadoop Inverted Index Generator
+
+## Description
+
+An inverted index is an index data structure storing a mapping from words to their documents(location) along with the frequency of occurrence. This repository contains two Programs that generate inverted indexes for a large corpus of documents using Hadoop. ```InvertedIndexJob.java``` is an implementation of an inverted index for unigrams using Hadoop MapReduce. ```BigramInvertedIndexJob.java``` is an implementation of an inverted index for bigrams using Hadoop MapReduce. GCP Dataproc clusters are used to run MapReduce jobs and generate inverted indexes.
+
+## Getting Started
+
+### Dependencies
+
+* Java
+* Apache Hadoop
+
+### Installing
+
+Use the following steps for installation.
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/dhananjaichand98/Hadoop_Inverted_Index_Generator.git
+   ```
+
+### Executing program
+
+These two java programs are uploaded as jar files to a GCP Dataproc cluster and MapReduce jobs are run on a corpus taken from the following link: https://drive.google.com/drive/u/1/folders/1Z4KyalIuddPGVkIm6dUjkpD_FiXyNIcq.
+
+## Data
+The program uses a subset of 74 files from a total of 408 files (text extracted from HTML tags) derived from the Stanford WebBase project that is available [here](https://ebiquity.umbc.edu/resource/html/id/351). It was obtained from a web crawl done in February 2007. It is one of the largest collections totaling more than 100 million web pages from more than 50,000 websites.
+
+## Authors
+
+- [Dhananjai Chand](https://www.linkedin.com/in/dhananjai-chand/)
+
+## Acknowledgment
+
+* [WordCount v1.0](https://hadoop.apache.org/docs/stable/hadoop-mapreduce-client/hadoop-mapreduce-client-core/MapReduceTutorial.html)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ src/InvertedIndexJob.java
@@ -0,0 +1,95 @@
+import java.io.IOException;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.HashMap;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class InvertedIndexJob {
+
+  public static class InvertedIndexMapper extends Mapper<Object, Text, Text, Text> {
+
+    private Text word = new Text();
+
+    private String preprocess(String text) {
+      text = text.replaceAll("[^A-Za-z]", " ");
+      text = text.toLowerCase();
+      return text;
+    }
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+      String splitVal[] = value.toString().split("\\t", 2);
+      Text docId = new Text(splitVal[0]);
+
+      String text = splitVal[1];
+      text = preprocess(text);
+      text = text.trim();
+      StringTokenizer tokenizer = new StringTokenizer(text);
+
+      while (tokenizer.hasMoreTokens()) {
+        word.set(tokenizer.nextToken());
+        context.write(word, docId);
+      }
+
+    }
+  }
+
+  public static class InvertedIndexReducer
+      extends Reducer<Text, Text, Text, Text> {
+    private Text result = new Text();
+
+    public void reduce(Text key, Iterable<Text> values,
+        Context context) throws IOException, InterruptedException {
+      
+      Map<String, Integer> m = new HashMap<>();
+
+      for (Text val : values) {
+        String docID = val.toString();
+        m.put(docID, m.getOrDefault(docID, 0) + 1);
+      }
+
+      StringBuilder sb = new StringBuilder();
+      for (Map.Entry<String, Integer> entry : m.entrySet()) {
+        sb.append(entry.getKey());
+        sb.append(":");
+        sb.append(entry.getValue().toString());
+        sb.append(" ");
+      }
+
+      result.set(sb.substring(0, sb.length() - 1));
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    if (args.length != 2) {
+      System.err.println("Usage: Inverted Index <input path> <output path>");
+      System.exit(-1);
+    }
+
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "inverted index");
+    job.setJarByClass(InvertedIndexJob.class);
+    job.setMapperClass(InvertedIndexMapper.class);
+    job.setReducerClass(InvertedIndexReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+    
+    String ipFile = args[0];
+    String opFile = args[1];
+    
+    FileInputFormat.addInputPath(job, new Path(ipFile));
+    FileOutputFormat.setOutputPath(job, new Path(opFile));
+
+    System.exit(job.waitForCompletion(true) ? 0 : -1);
+  }
+}
